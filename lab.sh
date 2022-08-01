1710f3b5b5b5043c91ad315e4343d55a4e7f2716
Thomas Antonini <antonini.thomas@gmail.com>
2022-08-01 07:58:16 +0200

    Add lab3
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- lab.sh
+++ lab.sh
@@ -5,10 +5,16 @@ set -eux
 BOOTLIN_YOCTO_DIR="$HOME/trainings/bootlin-yocto"
 POKY_DIR="$BOOTLIN_YOCTO_DIR/poky"
 BUILD_DIR="$BOOTLIN_YOCTO_DIR/build"
+BOOTLIN_YOCTO_LABS_DIR="$BOOTLIN_YOCTO_DIR/bootlin-yocto-labs"
 
 set +ux
 # shellcheck source=/dev/null
 source "$POKY_DIR/oe-init-build-env" "$BUILD_DIR"
 set -ux
+
+if [ ! -d "$POKY_DIR/meta/recipes-extended/ninvaders" ]; then
+	ln -s "$BOOTLIN_YOCTO_LABS_DIR"/ninvaders "$POKY_DIR/meta/recipes-extended/ninvaders"
+	bitbake ninvaders
+fi
 
 bitbake core-image-minimal
---
+++ ninvaders/ninvaders_0.1.1.bb
@@ -0,0 +1,3 @@
+require ninvaders.inc
+
+SRC_URI[sha256sum] = "bfbc5c378704d9cf5e7fed288dac88859149bee5ed0850175759d310b61fd30b"
