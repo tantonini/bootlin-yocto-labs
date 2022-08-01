df1e910d02df74cfc4bea06c6ef60a6a26601844
Thomas Antonini <antonini.thomas@gmail.com>
2022-08-01 07:46:28 +0200

    lab4: Add meta-bootlinlabs layer
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- lab.sh
+++ lab.sh
@@ -6,15 +6,23 @@ BOOTLIN_YOCTO_DIR="$HOME/trainings/bootlin-yocto"
 POKY_DIR="$BOOTLIN_YOCTO_DIR/poky"
 BUILD_DIR="$BOOTLIN_YOCTO_DIR/build"
 BOOTLIN_YOCTO_LABS_DIR="$BOOTLIN_YOCTO_DIR/bootlin-yocto-labs"
+META_BOOTLINLABS_DIR="$BOOTLIN_YOCTO_LABS_DIR/meta-bootlinlabs"
 
 set +ux
 # shellcheck source=/dev/null
 source "$POKY_DIR/oe-init-build-env" "$BUILD_DIR"
 set -ux
 
-if [ ! -d "$POKY_DIR/meta/recipes-extended/ninvaders" ]; then
-	ln -s "$BOOTLIN_YOCTO_LABS_DIR"/ninvaders "$POKY_DIR/meta/recipes-extended/ninvaders"
-	bitbake ninvaders
+# Remove layer added into poky at lab3
+if [ -d "$POKY_DIR/meta/recipes-extended/ninvaders" ]; then
+	rm "$POKY_DIR/meta/recipes-extended/ninvaders"
 fi
 
+# Layer created with
+# bitbake-layers create-layer "$META_BOOTLINLABS_DIR" -p 7
+
+# Layer added with
+# bitbake-layers add-layer "$META_BOOTLINLABS_DIR"
+
+bitbake ninvaders
 bitbake core-image-minimal
